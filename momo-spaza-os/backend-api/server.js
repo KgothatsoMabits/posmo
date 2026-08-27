23cc40f82aa2ddb4c2c3ea97fbb77bfa66751e82
CODENAMESOL3NYA <dultstersmusic@gmail.com>
2026-08-28 00:59:45 +0200

    chore:add morgan for logging

--- momo-spaza-os/backend-api/server.js
+++ momo-spaza-os/backend-api/server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
